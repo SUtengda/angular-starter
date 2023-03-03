e6f13931d413951b88f780cf4a3d9d857e00923e
SUtengda <303571072@qq.com>
2023-03-03 18:21:04 +0100

    tech: translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,46 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import localeFr from '@angular/common/locales/fr'
 import { registerLocaleData } from '@angular/common';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { CartModule } from './cart/cart.module';
 import { ProductsModule } from './products/products.module';
 
 registerLocaleData(localeFr);
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
-   ],
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en'
+    }),
     CartModule,
     ProductsModule,
   ],
   providers: [
     {
       provide: LOCALE_ID,
-      useValue: 'fr-FR' 
+      useValue: 'fr-FR'
     }
   ],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PriceWithTaxPipe } from './pipes/priceWithTax.pipe';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule
   ],
   declarations: [
     PriceWithTaxPipe  
   ],
   exports: [
-    PriceWithTaxPipe
+    PriceWithTaxPipe,
+    TranslateModule
   ]
 })
 export class SharedModule { }
